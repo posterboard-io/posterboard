692932795a60d5db161ac5b86f0fdc42600f9ec5
Tyler K <tylerkruer1@gmail.com>
2023-12-15 20:45:28 -0700

    automated puhs

--- src/components/pb/global/footer.tsx
+++ src/components/pb/global/footer.tsx
@@ -10,9 +10,7 @@ import { DiscordLogoIcon, GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/reac
 
 export default function Footer() {
     const posterboardClearImage: StaticImageData = posterboardClear
-    const discordSVG: StaticImageData = discordLogo
-    const githubSVG: StaticImageData = githubLogo
-    const xSVG: StaticImageData = xLogo
+    
     return (        
         <div style={{ position: 'relative' }}>        
             <footer style={{ position: 'relative', zIndex: 1 }}>
@@ -27,6 +25,19 @@ export default function Footer() {
                         }}
                     />                
                 </div>
+
+                <div
+                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                    aria-hidden="true"
+                >
+                    <div
+                        className="relative left-[calc(50%+5rem)] aspect-[1155/678] w-[18rem] -translate-x-1/2 rotate-[-30deg] bg-gradient-to-tl from-[#3e82ff] to-[#0084ff] opacity-30 sm:left-[calc(50%+15rem)] sm:w-[36rem]"
+                        style={{
+                            clipPath: 'polygon(25.9% 44.1%, 0% 61.6%, 2.5% 26.9%, 14.5% 0.1%, 19.3% 2%, 27.5% 32.5%, 39.8% 62.4%, 47.6% 68.1%, 52.5% 58.3%, 54.8% 34.5%, 72.5% 76.7%, 99.9% 64.9%, 82.1% 100%, 72.4% 76.8%, 23.9% 97.7%, 25.9% 44.1%)',
+                        }}
+                    />                
+                </div>
+                
                 <section className="py-8">                
                     <div className="container mx-auto px-2 py-2 flex flex-wrap justify-between items-center">
                         <div className="w-full sm:w-auto mb-2 sm:mb-0 flex justify-between items-center"> 
--- src/components/pb/landing/landing-count-up-jobs.tsx
+++ src/components/pb/landing/landing-count-up-jobs.tsx
@@ -36,7 +36,7 @@ export default function LandingCountUpJobs() {
                     text: "Get Searching",
                     className: "rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600",                    
                 }}
-                showButton={true}
+                showButton={false}
             />
             <CountUpCard 
                 countUpTo={randomValue2}
